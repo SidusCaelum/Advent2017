26291814ed83f1ea645c86f477150ec08550c531
Joshua Anthony de Leon <jdeleon@scires.com>
2017-12-01 14:41:38 -0500

    final commit before leaving work

--- day1.py
+++ day1.py
@@ -1,8 +1,7 @@
 def calculate(testInput):
+    skip = len(testInput) / 2
     total = 0
     for i in range(0, len(testInput)):
-        print("ind: {0}", i)
-
         if(i == (len(testInput)-1)):
             if(testInput[i] == testInput[0]):
                 total += int(testInput[i])
